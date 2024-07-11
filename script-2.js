71e0ea74459a43bdccc17c461d4908f1da0a837b
tachyon <turone2021@gmail.com>
2024-07-10 22:01:16 -0500

    7-10-24 10:01 pm step 128

--- script-2.js
+++ script-2.js
@@ -163,9 +163,24 @@ function attack() {
   text.innerText = "The "+ monsters[fighting].name +" attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + "."
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp);
+healthText.innerText = health;
+monsterHealthText.innerText = monsterHealth;
+if ( health <= 0){
+  lose() 
+} else if (monsterHealth <=0 ){
+  defeatMonster()
+}
 }
 
 function dodge() {
 
+}
+
+function defeatMonster(){
+
+}
+
+function lose() {
+  
 }
