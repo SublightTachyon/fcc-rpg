b9b3bb0d285d89ce5f13130b97c9476068dd49ca
Tachyon <turone2021@gmail.com>
2024-07-18 17:56:04 -0500

    5:55 pm 7-18-24 step 168

--- script-2.js
+++ script-2.js
@@ -248,4 +248,7 @@ function pick(guess) {
   while (numbers.length < 10){
     numbers.push(Math.floor(Math.random() * 11))
   }
+  text.innerText = "You picked "+ guess +". Here are the random numbers: \n"
+  for (let i = 0;  i< 10; i++) {
+    text.innerText += numbers[i]  }
 }
